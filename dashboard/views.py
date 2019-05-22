b3aed951756a73567ebe7339a503d6159bad4050
fbormann <felipe.bormann@gmail.com>
2019-05-22 19:08:15 -0300

    refactor(dashboard/views.py): add error handling

--- dashboard/views.py
+++ dashboard/views.py
@@ -4,8 +4,12 @@ from django.template import loader
 
 
 def index(request):
-    template = loader.get_template('dashboard/index.html')
-    context = {
-        "page_title": "Guaraci - Data Visualization Tool"
-    }
-    return HttpResponse(template.render(context, request))
+    try:
+        template = loader.get_template('dashboard/index.html')
+        context = {
+            "page_title": "Guaraci - Data Visualization Tool"
+        }
+        return HttpResponse(template.render(context, request))
+    except Exception as e:
+        print(e)
+        return HttpResponse("we were not able to provide this request")
